a105bed602c1cc9f4e3d73ffbfc193910acb4180
eelhilali <eelhilali@users.noreply.github.com>
2019-02-19 11:00:21 +0100

    Add files via upload

--- EmailValidation.Tests/RegexUtilitiesShould.cs
+++ EmailValidation.Tests/RegexUtilitiesShould.cs
@@ -6,23 +6,25 @@ namespace EmailValidation.Tests
     [TestClass]
     public class RegexUtilitiesShould
     {
-        private readonly List<string> emailAddresses = new List<string>()
+        private readonly string[] emailAddresses =
         {
-            "david.jones@proseware.com",//0
-            "d.j@server1.proseware.com",//1
-            "jones@ms1.proseware.com",//2
-            "j.@server1.proseware.com",//3
-            "j@proseware.com9",//4
-            "js#internal@proseware.com",//5
-            "j_9@[129.126.118.1]",//6
-            "j..s@proseware.com",//7
-            "js*@proseware.com",//8
-            "js@proseware..com",//9
-            "js@proseware.com9",//10
-            "j.s@server1.proseware.com",//11
-            "\"j\\\"s\\\"\"@proseware.com",//12
-            "js@contoso.中国",//13
-            "example+a@gmail.com"//14
+            // Valid
+            "david.jones@proseware.com",
+            "d.j@server1.proseware.com",
+            "jones@ms1.proseware.com",            
+            "j@proseware.com9",
+            "js#internal@proseware.com",
+            "j_9@[129.126.118.1]",     
+            "js@proseware.com9",
+            "j.s@server1.proseware.com",
+            "\"j\\\"s\\\"\"@proseware.com",
+            "js@contoso.中国",
+            "example+a@gmail.com",
+            //Invalid
+            "j.@server1.proseware.com",
+             "j..s@proseware.com",
+            "js*@proseware.com",
+            "js@proseware..com"
         };
 
         [TestMethod]
@@ -53,12 +55,12 @@ namespace EmailValidation.Tests
         }
 
         [TestMethod]
-        public void Return_false_when_the_email_is_email3()
+        public void Return_true_when_the_email_is_email3()
         {
             var emailAddress = emailAddresses[3];
 
             var result = RegexUtilities.IsValidEmail(emailAddress);
-            Assert.IsFalse(result);
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
@@ -89,30 +91,30 @@ namespace EmailValidation.Tests
         }
 
         [TestMethod]
-        public void Return_false_when_the_email_is_email7()
+        public void Return_true_when_the_email_is_email7()
         {
             var emailAddress = emailAddresses[7];
 
             var result = RegexUtilities.IsValidEmail(emailAddress);
-            Assert.IsFalse(result);
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
-        public void Return_false_when_the_email_is_email8()
+        public void Return_true_when_the_email_is_email8()
         {
             var emailAddress = emailAddresses[8];
 
             var result = RegexUtilities.IsValidEmail(emailAddress);
-            Assert.IsFalse(result);
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
-        public void Return_false_when_the_email_is_email9()
+        public void Return_true_when_the_email_is_email9()
         {
             var emailAddress = emailAddresses[9];
 
             var result = RegexUtilities.IsValidEmail(emailAddress);
-            Assert.IsFalse(result);
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
@@ -125,39 +127,39 @@ namespace EmailValidation.Tests
         }
 
         [TestMethod]
-        public void Return_true_when_the_email_is_email11()
+        public void Return_false_when_the_email_is_email11()
         {
             var emailAddress = emailAddresses[11];
 
             var result = RegexUtilities.IsValidEmail(emailAddress);
-            Assert.IsTrue(result);
+            Assert.IsFalse(result);
         }
 
         [TestMethod]
-        public void Return_true_when_the_email_is_email12()
+        public void Return_false_when_the_email_is_email12()
         {
             var emailAddress = emailAddresses[12];
 
             var result = RegexUtilities.IsValidEmail(emailAddress);
-            Assert.IsTrue(result);
+            Assert.IsFalse(result);
         }
 
         [TestMethod]
-        public void Return_true_when_the_email_is_email13()
+        public void Return_false_when_the_email_is_email13()
         {
             var emailAddress = emailAddresses[13];
 
             var result = RegexUtilities.IsValidEmail(emailAddress);
-            Assert.IsTrue(result);
+            Assert.IsFalse(result);
         }
 
         [TestMethod]
-        public void Return_true_when_the_email_is_email14()
+        public void Return_false_when_the_email_is_email14()
         {
             var emailAddress = emailAddresses[14];
 
             var result = RegexUtilities.IsValidEmail(emailAddress);
-            Assert.IsTrue(result);
+            Assert.IsFalse(result);
         }
     }
 }
