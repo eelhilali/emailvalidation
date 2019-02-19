3c5f9ce0d7f242e310e97775290f3016dd058ffe
eelhilali <eelhilali@users.noreply.github.com>
2019-02-19 10:12:11 +0100

    First commit EmailValidation csproj

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmailValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Email validation!");
+            Console.ReadLine();
+        }
+    }
+}
